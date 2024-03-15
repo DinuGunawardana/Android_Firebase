0e6e1c4c916d296cd692a01836e1e94cd84e9431
DinuGunawardana <62100212+DinuGunawardana@users.noreply.github.com>
2024-03-15 11:27:38 +0530

    Add Quote activity

---
+++ app/src/main/java/com/example/android_firebase/AddQuoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddQuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_quote);
+    }
+}
